a0f64bdd4a2ce1566dea0adc5afa6737c5f98b0b
Sara Collins <datadivallc@gmail.com>
2018-01-02 14:43:47 -0600

    Added functions to get file paths.

--- test/unit_tests/test_vehicle_detection.py
+++ test/unit_tests/test_vehicle_detection.py
@@ -7,10 +7,10 @@ class TestVehicleDetection(unittest.TestCase):
     def setUp(self):
         self.config = vehdetect.load_config('test_configuration.json')
 
-    def test_something(self):
-        path = self.config['image_file_pattern']
+    def test_get_image_file_paths(self):
+        path = self.config['nonvehicles_image_directory']
         image_list = vehdetect.get_image_file_paths(path)
-        self.assertEqual(len(image_list), 6)
+        self.assertEqual(12, len(image_list))
 
     def tearDown(self):
         del self.config
--- vehicle_detection.py
+++ vehicle_detection.py
@@ -2,10 +2,13 @@ import logging
 import argparse
 import json
 import glob
+import sys
+import os
 
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 
 def parse_args(arguments):
     """
@@ -19,6 +22,7 @@ def parse_args(arguments):
 
     return vars(parser.parse_args(arguments))
 
+
 def load_config(config_name):
     """
     loads a json config file and returns a config dictionary
@@ -27,10 +31,34 @@ def load_config(config_name):
         configuration = json.load(config_file)
         return configuration
 
-def get_image_file_paths(pattern):
+
+def get_image_file_paths(directory):
     """
 
-    :param pattern:
-    :return:
+    :param directory:
+    :return: list of image file paths
     """
-    return glob.glob(pattern)
+    image_types = os.listdir(directory)
+    image_types = [imtype for imtype in image_types if not imtype.startswith('.')]
+
+    image_list = []
+
+    [image_list.extend(glob.glob(directory + imtype + '/*')) for imtype in image_types]
+
+    return image_list
+
+
+if __name__ == '__main__':
+
+    # Set TensorFlow logging so it isn't so verbose.
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+    logger.info("Reading configuration file...")
+    args = parse_args(sys.argv[1:])
+    config = load_config(args['config'])
+
+    logger.info("Reading image file lists...")
+    cars = get_image_file_paths(config['vehicles_image_directory'])
+    notcars = get_image_file_paths(config['nonvehicles_image_directory'])
+
+    sys.exit(0)
